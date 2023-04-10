854659008302dc444ea6b23662569cb6efbeceaf
Iuliia Shikhanova <yshihanova@gmail.com>
2023-04-10 13:06:12 +0100

    add: auth0 and hook with properties

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './style/tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider 
+    domain="dev-7wmznkm41wx0ichb.us.auth0.com"
+    clientId="uk0msY5QXRml3wLU90f10nsZ8jUw3KSk"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
